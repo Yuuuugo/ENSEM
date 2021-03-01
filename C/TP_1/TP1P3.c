16a79a1b6ee518f923f1f9e517fbb7a27d7d8dcb
Yuuuugo <hugogabrielidis@gmail.com>
2021-03-01 04:09:43 +0100

    Fin TP1

--- C/TP_1/TP1P3.c
+++ C/TP_1/TP1P3.c
@@ -6,45 +6,44 @@
 
 #define MAX_TAILLE 100
 typedef struct liste
-{
+{   
     int valeurs[MAX_TAILLE];
     int taille;
 }liste;
 
-void afficher_tableau(liste l){
-    for (int i = 0 ; i < l.taille ; i++)
+void afficher_tableau(liste *l){
+    for (int i = 0 ; i < (*l).taille ; i++)
     {
-        if(i< l.taille-1){
-        printf("%d ", l.valeurs[i]);
+        if(i< (*l).taille-1){
+        printf("%d ", (*l).valeurs[i]);
         }
         else{
-            printf("%d \n", l.valeurs[i]);
+            printf("%d \n", (*l).valeurs[i]);
         }
     }
 }
 
-liste generer_tableau(liste l){
+liste generer_tableau(liste *l){
     int i;
     srand(time(NULL));
-    for(i = 0; i <=l.taille; i++){
-        l.valeurs[i] =  rand()%l.taille+50;        
+    for(i = 0; i <=(*l).taille; i++){
+        (*l).valeurs[i] =  rand()%(*l).taille+1;        
     }
-
 }
 
 
 
-liste permuter(liste l,int indice1,int indice2){
-    int x = l.valeurs[indice1];
-    l.valeurs[indice1] = l.valeurs[indice2];
-    l.valeurs[indice2] = x;
+liste permuter(liste *l,int indice1,int indice2){
+    int x = (*l).valeurs[indice1];
+    (*l).valeurs[indice1] = (*l).valeurs[indice2];
+    (*l).valeurs[indice2] = x;
 }
 
-liste trier(liste l){
-    for(int i =0; i<l.taille;i++){
-            for(int j =i+1;j<l.taille;j++)
+liste trier(liste *l){
+    for(int i =0; i<(*l).taille;i++){
+            for(int j =i+1;j<(*l).taille;j++)
             {
-                if(l.valeurs[j] < l.valeurs[i])
+                if((*l).valeurs[j] < (*l).valeurs[i])
                 {
                     permuter(l,i,j);
                 }
@@ -55,11 +54,14 @@ liste trier(liste l){
     
 
 
+
 void main(){
-    liste tableau ;
-    tableau.taille = 3;
-    afficher_tableau(tableau);
-    generer_tableau(tableau);
-    trier(tableau);
-    afficher_tableau(tableau);
+    struct liste tableau ;
+    tableau.taille = 8;
+    printf("Voici un tableau de taille %u avec des valeurs generes aleatoirement entre 1 et %u \n",tableau.taille,tableau.taille);
+    generer_tableau(&tableau);
+    afficher_tableau(&tableau);
+    trier(&tableau);
+    printf("Voici le tableau tries en ordre croissant\n"); 
+    afficher_tableau(&tableau);
 }

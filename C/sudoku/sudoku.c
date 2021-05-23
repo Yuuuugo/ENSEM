f85564eac8f66168957e62dab6e739be51f999a8
Yuuuugo <hugogabrielidis@gmail.com>
2021-05-23 16:54:49 +0200

    argc

--- C/sudoku/sudoku.c
+++ C/sudoku/sudoku.c
@@ -2,7 +2,23 @@
 
 
 
-int main() {
+int main(int argc , char * argv[]) {
+    if(strcmp(argv[1],"standalone")){
+        printf("%s \n",argv[1]);
+    }
+    else if(strcmp(argv[1],"-multithread") ){
+        printf("%s \n",argv[1]);
+    }
+    else if(strcmp(argv[1],"-server")){
+        printf("%s \n",argv[1]);
+    }
+    else{
+        printf("-standalone : lancer le programme en mode basique \n");
+        printf("-multithread : lancer le programme en mode multi-thread \n");
+        printf("-server : lancer le programme en mode serveur \n");
+    }
+    
+    /*
     sudo_t sudo,test;
     lire_plateau("sudoku1.txt",&sudo);
     afficher_plateau(sudo);
@@ -13,4 +29,6 @@ int main() {
     afficher_plateau(sudo);
     ecriture(sudo,sudo.nom);
     printf("%u \n",resolu(sudo));
+    */
+
 }

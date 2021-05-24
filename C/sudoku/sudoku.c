bfb8dae48b2614287e0aaaf69530ea8122570f67
Yuuuugo <hugogabrielidis@gmail.com>
2021-05-24 14:32:54 +0200

    Fin partie1

--- C/sudoku/sudoku.c
+++ C/sudoku/sudoku.c
@@ -1,15 +1,24 @@
 #include "sudoku.h"
 
 
-
 int main(int argc , char * argv[]) {
-    if(strcmp(argv[1],"standalone")){
+    if(strcmp(argv[1],"-standalone")==0){
+        char nom[11] = {0};
+        sudo_t sudo;
+        printf("Rentrer le nom du sudoku que vous voulez resoudre:");
+        scanf("%s",nom);
+        printf("%s \n",nom);
+        lire_plateau(nom,&sudo);
+        afficher_plateau(sudo);
+        sudo = convertisseur(sudo);
+        resolution_Backtrack(&sudo);
+        afficher_plateau(sudo);
+        ecriture(sudo,sudo.nom);
+    }
+    else if(strcmp(argv[1],"-multithread")==0){
         printf("%s \n",argv[1]);
     }
-    else if(strcmp(argv[1],"-multithread") ){
-        printf("%s \n",argv[1]);
-    }
-    else if(strcmp(argv[1],"-server")){
+    else if(strcmp(argv[1],"-server")==0){
         printf("%s \n",argv[1]);
     }
     else{
@@ -17,18 +26,6 @@ int main(int argc , char * argv[]) {
         printf("-multithread : lancer le programme en mode multi-thread \n");
         printf("-server : lancer le programme en mode serveur \n");
     }
-    
-    /*
-    sudo_t sudo,test;
-    lire_plateau("sudoku1.txt",&sudo);
-    afficher_plateau(sudo);
-    printf("%s",sudo.nom);
-    sudo = convertisseur(sudo);
-    afficher_plateau(sudo);
-    resolution_Backtrack(&sudo);
-    afficher_plateau(sudo);
-    ecriture(sudo,sudo.nom);
-    printf("%u \n",resolu(sudo));
-    */
+
 
 }
